b18369b27b64dc048a05070e812d8e4e7485423e
consul <2301488793@qq.com>
2018-10-23 17:32:59 +0800

    backup

--- app/src/main/java/com/practice/kotlinandroid/MainActivity.kt
+++ app/src/main/java/com/practice/kotlinandroid/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.practice.kotlinandroid
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 val MainActivity.tv2: TextView
     get() = findViewById(R.id.tv)
 var HAHA: String = ""
     set(value) {
-        field = value
         Log.e("TAG", value)
     }
-    get() = "hello"
+    get() {
+        Log.e("TAG", field)
+        return "hello"
+    }
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +30,23 @@ class MainActivity : AppCompatActivity() {
         tv.visibility = View.VISIBLE;
         tv.text = "haha"
         tv2.text = "haha2"
-        tv2.setOnClickListener { (it as TextView).text = "haha3" }
+        tv2.setOnClickListener { v: View ->
+            AlertDialog.Builder(this@MainActivity)
+                .setMessage("haha")
+                .setPositiveButton("ok") { v1, v2 ->
+                    Toast.makeText(this@MainActivity, "Positive", Toast.LENGTH_LONG).show()
+                }
+                .setNegativeButton("cancel") { v1, v2 ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Negative",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .create()
+                .show()
+            (v as TextView).text = "haha3";
+        }
         HAHA = "test"
     }
 
